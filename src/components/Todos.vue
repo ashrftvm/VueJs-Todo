62f5d8acfb55f3a9de5546db1c407a975c7715f3
Ashrf Ali <ashrftvm1@gmail.com>
2019-08-27 13:39:07 +0400

    nested components and data passing

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <div v-bind:key="todo.id" v-for="todo in todos">{{todo.title}}</div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" />
+    </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem";
 export default {
   name: "Todos",
+  components: {
+    TodoItem
+  },
   props: ["todos"]
 };
 </script>
